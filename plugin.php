82da927ec5941999011593fc38c8c4735f9c1b9b
Toby Zerner <toby.zerner@gmail.com>
2014-05-10 13:34:46 +0930

    require esoTalk 1.0.0g4

--- plugin.php
+++ plugin.php
@@ -11,7 +11,10 @@ ET::$pluginInfo["Likes"] = array(
 	"author" => "Toby Zerner",
 	"authorEmail" => "support@esotalk.org",
 	"authorURL" => "http://esotalk.org",
-	"license" => "GPLv2"
+	"license" => "GPLv2",
+	"dependencies" => array(
+		"esoTalk" => "1.0.0g4"
+	)
 );
 
 class ETPlugin_Likes extends ETPlugin {
